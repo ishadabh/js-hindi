cd92411ee78eef3edc56450582f646f2413f012c
Shadab Hussain <shadabh255@gmail.com>
2026-08-20 18:51:10 +0000

    datatype done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) it has differrnt syntax in node but work perfectly in browser's console
+
+// code readability shuold be high
+
+let name = "hitesh";
+let num = 32;
+let isLoggedIn = false;
+
+// number -> 2 to pow 53
+// bigint 
+// string -> "" or ''
+// boolean -> true/false
+// null -> standalone value (representaion of emty value) and object 
+//undefined -> when value is not assigned
+// symbol -> for unique 
+
+
+// non-premitives
+// object 
+
+console.log(typeof("shadab"));
+console.log(typeof(null)); // object
