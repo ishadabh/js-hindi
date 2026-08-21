afdbab053d3a7ec39204300922b6542ba0a15c26
Shadab Hussain <shadabh255@gmail.com>
2026-08-21 20:30:21 +0000

    operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -73,3 +73,46 @@ let stringNum = String(num);
 console.log(typeof(stringNum));
 console.log(stringNum);
 
+
+// ******************* Operations *****************
+
+
+let val = 3;
+let negVal = -val;
+console.log(negVal);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); // 2 to power 3
+console.log(2/2);
+
+
+
+let str1 = "hello";
+let str2 = " Shadab";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("1" + 2); // 12 
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2 "); // 32
+
+
+console.log(true);
+console.log(+true);
+// console.log(true+); error
+
+console.log(+"");  // 0
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1,num2,num3);
+
+
+
+
