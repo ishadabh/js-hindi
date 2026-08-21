6c59f0e034191f68f6a76b8e0a6679331c109826
Shadab Hussain <shadabh255@gmail.com>
2026-08-21 20:51:07 +0000

    dataype done

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,16 @@
+// Premitive 7 in number (call by value)
+
+// string 
+// number
+// boolean
+// null 
+// undefind 
+// symbol 
+// BigInt
+
+
+// Reference Type Or non-primitive (call by reference)
+
+// array
+// Objects (imp)
+// Functions
