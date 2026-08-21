20a69c2388252e22e82a0844b3867d29758217e5
Shadab Hussain <shadabh255@gmail.com>
2026-08-21 21:10:31 +0000

    more on datatypes

--- 01_basics/05_datatypes.js
+++ 01_basics/05_datatypes.js
@@ -6,11 +6,38 @@
 // null 
 // undefind 
 // symbol 
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(typeof(id));
+console.log(id == anotherId);
+
+
+
 // BigInt
+const bigNumber = 1345738920771647789908098n
+console.log(typeof(bigNumber));
+
 
 
 // Reference Type Or non-primitive (call by reference)
 
 // array
-// Objects (imp)
+let heros = ["h1", "h2", "h3"];
+console.log(typeof(heros));
+
+// Objects (imp) -> key : value
+let myObj={
+    name: "Shadab",
+    age : 24
+}
+console.log(typeof(myObj));
+
+
 // Functions
+
+// as a variable 
+const myFunction = function(){
+    console.log("hero");  
+}
+console.log(typeof(myFunction));
