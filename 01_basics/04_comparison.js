d583b4dbba48e31d8ee1a2f04a268f4c88df40c5
Shadab Hussain <shadabh255@gmail.com>
2026-08-21 20:42:46 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(3 >= 4);
+
+
+console.log("2" > 1);
+console.log("05" >= 4);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined >= 0);
+
+
+// ************************ Strict check ===
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false because it also check datatype
+
+
