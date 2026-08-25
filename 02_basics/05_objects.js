eed3eb7f6744dfa06ee0a0cdcf8965b964d66aa5
Shadab Hussain <shadabh255@gmail.com>
2026-08-25 12:58:23 +0000

    object destructuring done

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,15 @@
+let course = {
+    coursename : "js in hindi",
+    rice : "999",
+    courseInstructor: "Shadab"
+}
+
+// object destructure
+let {courseInstructor: instructor} = course;
+console.log(instructor);
+
+
+const navbar = () => {
+
+}
+navbar(company = "shadab")
