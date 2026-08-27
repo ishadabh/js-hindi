0622ba5d32d02ec9564d56bc99a35aa74b1a0392
Shadab Hussain <shadabh255@gmail.com>
2026-08-27 11:06:22 +0000

    execution steps done

---
+++ 03_basics/jsExecution.js
@@ -0,0 +1,14 @@
+// Javascript execution context
+
+
+// 1. sabse pahle {} global execution banta h aur this ko assign kr diya jata h
+// broswer me gloabal execution context thora alag hota, node.js ka thora alag hota h
+// browser me this ki value window object hota h
+
+// js is a single threaded, every thing is a process 
+
+
+// steps 
+// 2. Memory creation phase
+// 3. Execution phase
+// 
